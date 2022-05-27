806465341201a541a24132453f69d3ae30ead569
Bartosz Ratajczyk <b.ratajczyk124@gmail.com>
2022-05-28 01:22:55 +0200

    Adding simple display for quick check

--- FedditAndroid/app/src/main/java/com/example/fedditandroid/profile_Fragment.java
+++ FedditAndroid/app/src/main/java/com/example/fedditandroid/profile_Fragment.java
@@ -1,20 +1,82 @@
 package com.example.fedditandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class profile_Fragment extends Fragment {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
+
+    private TextView fullNameText, emailText;
+    private Button logout;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_home_, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile_, container, false);
+
+
+        fullNameText = (TextView) v.findViewById(R.id.profileFullNameID);
+        emailText = (TextView) v.findViewById(R.id.profileEmailID);
+        logout = (Button) v.findViewById(R.id.profileLogoutID);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+
+                    fullNameText.setText(fullName);
+                    emailText.setText(email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "Something wrong happened!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            }
+        });
+
         return v;
+
     }
 }
