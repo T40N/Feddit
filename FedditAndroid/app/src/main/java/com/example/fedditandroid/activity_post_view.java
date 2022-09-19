0bcb9d66efc5a17dfce06391cd76c942fe0ce0d3
MikolajjalokiM <sanecki.mikolaj@vp.pl>
2022-09-19 18:50:57 +0200

    update

--- FedditAndroid/app/src/main/java/com/example/fedditandroid/activity_post_view.java
+++ FedditAndroid/app/src/main/java/com/example/fedditandroid/activity_post_view.java
@@ -1,61 +1,51 @@
 package com.example.fedditandroid;
 
-import android.content.Intent;
-import android.os.Bundle;
-
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
-import java.util.ArrayList;
-
-
-public class home_Fragment extends Fragment implements AdapterView.OnItemSelectedListener{
-
-    Button addPostButton;
+public class activity_post_view extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private FirestoreRecyclerOptions<Post> options;
     private FirestoreRecyclerAdapter<Post, PostViewHolder> adapter;
     private RecyclerView recyclerView;
     private CollectionReference dbRef;
+    private Button backBtn;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_home_, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_view);
 
         dbRef = FirebaseFirestore.getInstance().collection("Posts");
+        String uId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        Query query = dbRef.orderBy("likeCounter").whereEqualTo("userPostID", uId).limit(50);
 
-        Query query = dbRef.orderBy("likeCounter").limit(50);
-
-        recyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+        recyclerView = (RecyclerView) findViewById(R.id.PostsRecycleView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity_post_view.this));
 
         options = new FirestoreRecyclerOptions.Builder<Post>()
                 .setQuery(query, Post.class)
@@ -98,31 +88,27 @@ public class home_Fragment extends Fragment implements AdapterView.OnItemSelecte
         };
 
         adapter.startListening();
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(v.getContext(), DividerItemDecoration.VERTICAL);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(activity_post_view.this, DividerItemDecoration.VERTICAL);
         dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.seperator));
         recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setAdapter(adapter);
 
+        backBtn = (Button) findViewById(R.id.returnHomeBtn);
 
-        addPostButton = (Button) v.findViewById(R.id.addPostButtonID);
-
-        addPostButton.setOnClickListener(new View.OnClickListener() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity(), AddTask.class));
+                startActivity(new Intent(activity_post_view.this, profile_Fragment.class));
             }
         });
 
-        Spinner sortSpinner = (Spinner) v.findViewById(R.id.sort_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+        Spinner sortSpinner = (Spinner) findViewById(R.id.sort_spinner_1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.sort_options, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sortSpinner.setAdapter(adapter);
 
 
-
-
-        return v;
     }
 
     @Override
